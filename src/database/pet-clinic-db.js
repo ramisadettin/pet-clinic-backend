d047cecd77555a8c5340b68d116aa37ae27c45a5
lagger94hub <ramisaadaldden@gmail.com>
2023-11-01 06:40:48 +0300

    modifying env

--- src/database/pet-clinic-db.js
+++ src/database/pet-clinic-db.js
@@ -1,14 +1,9 @@
-var connData = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? {
-  host: 'localhost', 
-  user: 'root',
-  password: 'petApp12345!',
-  database: 'pet_clinic',
-  timezone: 'Z'
-} : {
+var connData = {
   host: process.env.DB_HOST, 
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306,  
+  port: process.env.APP_DB_PORT,  
+  timezone: 'Z'
 }
 export default connData
